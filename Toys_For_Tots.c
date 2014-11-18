063f7819cac5130f355c7bb2bfc5dd213f732356
Liam Heisler <liamheisler@aol.com>
2014-11-18 15:52:56 -0800

    Create Toys_For_Tots.c

---
+++ Toys_For_Tots.c
@@ -0,0 +1,83 @@
+/*
+  Toys_For_Tots.c
+  This program will enable Roberta to pick a present up off a table.  
+*/
+
+#include "simpletools.h"                      // Include simple tools
+#include "servo.h"                            //this program uses servo motors
+
+//Assign pins to the different body parts
+#define LeftKnee 1
+#define RightKnee 2
+#define LeftHip 6
+#define RightHip 7
+#define LeftRotator 12
+#define LeftShoulder 13
+#define LeftElbow 14
+#define RightRotator 15
+#define RightShoulder 16
+#define RightElbow 17
+#define Head 8
+
+//Function Declarations
+     void SetMotorSpeed(void);                 //Adjusts the speed at which limbs will move
+     void startingPos(void);
+     void squeezePres(void);
+     void liftPres(void);
+ int speed = 5;                               //sets the speed at which servos will turn
+  
+int main()                                    // Main function
+{
+   startingPos();
+   SetMotorSpeed();
+   pause(5000);
+   squeezePres();
+   pause(5000);
+   liftPres();
+}
+
+void startingPos()
+{
+  servo_angle(LeftRotator, 0);
+  servo_angle(LeftShoulder, 180);
+  servo_angle(LeftElbow, 1750); 
+  servo_angle(RightRotator, 1830); 
+  servo_angle(RightShoulder, 1730); 
+  servo_angle(RightElbow, 0);
+}
+
+void squeezePres()
+{
+  servo_angle(LeftRotator, 1200);
+  servo_angle(LeftShoulder, 530);
+  servo_angle(LeftElbow, 1750); 
+  servo_angle(RightRotator, 840); 
+  servo_angle(RightShoulder, 1350); 
+  servo_angle(RightElbow, 0);
+}
+
+void liftPres()
+{
+  servo_angle(LeftRotator, 1300);
+  servo_angle(LeftShoulder, 1700);
+  servo_angle(LeftElbow, 1750); 
+  servo_angle(RightRotator, 850); 
+  servo_angle(RightShoulder, 320); 
+  servo_angle(RightElbow, 0);
+}
+
+void SetMotorSpeed(void)
+{
+   
+   servo_setramp(LeftHip,speed);
+   servo_setramp(LeftKnee,speed);
+   servo_setramp(LeftRotator,speed);
+   servo_setramp(LeftShoulder,speed);
+   servo_setramp(LeftElbow,speed);
+   
+   servo_setramp(RightHip,speed);
+   servo_setramp(RightKnee,speed);
+   servo_setramp(RightRotator,speed);
+   servo_setramp(RightShoulder,speed);
+   servo_setramp(RightElbow,speed); 
+}
